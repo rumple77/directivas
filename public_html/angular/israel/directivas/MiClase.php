0aa46eb9f2d0b2321745bbb75af5b27c132fc339
rumple77 <sergio2ges@gmail.com>
2015-02-10 09:49:34 +0100

    subo nuevos cambios

--- public_html/angular/israel/directivas/MiClase.php
+++ public_html/angular/israel/directivas/MiClase.php
@@ -2,8 +2,17 @@
 
 class MiClase {
 
+    private $var2;
+    private $var3;
+            
     function __construct() {
         
+        $this->var2 = 'mi variable';
+    }
+    
+    public function mostrarMensaje() {
+        
+        echo $this->var2;
     }
 
 }
